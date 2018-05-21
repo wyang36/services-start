0f9ac6d5a3a59d7429e1710f48202d67a80eb991
Kira Yang <wenqin.z.yang@gmail.com>
2018-05-21 09:06:15 -0400

    Initial commit

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+  //providers: [LoggingService]
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+
+  constructor(private loggingSerice: LoggingService, private accountsService: AccountsService) { }
+
+  onSetTo(status: string) {
+    this.accountsService.updateStatus(this.id, status);
+    //this.loggingSerice.logStatusChange(status);
+  }
+}
---
+++ src/app/accounts.service.ts
@@ -0,0 +1,28 @@
+import { LoggingService } from "./logging.service";
+
+export class AccountsService {
+    accounts = [
+        {
+            name: 'Master Account',
+            status: 'active'
+        },
+        {
+            name: 'Testaccount',
+            status: 'inactive'
+        },
+        {
+            name: 'Hidden Account',
+            status: 'unknown'
+        }
+    ];
+
+    constructor(private loggingService: LoggingService) { };
+
+    addAccount(name: string, status: string) {
+        this.accounts.push({ name: name, status: status });
+    }
+
+    updateStatus(id: number, status: string) {
+        this.accounts[id].status = status;
+    }
+}
